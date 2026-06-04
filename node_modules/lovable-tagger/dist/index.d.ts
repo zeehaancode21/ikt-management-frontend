82e1a324fa4a0d86e19dca41e0b3373b184e91d4
zeehaancode21 <zeehaancode@gmail.com>
2026-06-04 16:41:06 +0530

    Remove node_modules from repository

--- node_modules/lovable-tagger/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from 'vite';
-
-interface LovableTaggerOptions {
-    jsxSource?: boolean;
-    tailwindConfig?: boolean;
-}
-declare function lovableTagger({ jsxSource, tailwindConfig, }?: LovableTaggerOptions): Plugin;
-
-export { type LovableTaggerOptions, lovableTagger as componentTagger };
